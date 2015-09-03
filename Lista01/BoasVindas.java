1719590ea6e22077b1f260127a9fb0395b457cc1
Wallyson Galvão <wallyson.galvao@gmail.com>
2015-09-02 23:20:40 -0300

    Boas Vindas

---
+++ Lista01/BoasVindas.java
@@ -0,0 +1,17 @@
+/*
+ * Escreva um programa que imprima a mensagem "Bem vindo!" para o usuário.
+ */
+package Lista01;
+
+//1. Crie uma classe chamada BoasVindas.
+public class BoasVindas {
+    // 2. Crie um bloco main (Dica: digite psvm e pressione a tecla TAB).
+    public static void main(String[] args) {
+        /* 3. Dentro do main, utilize a classe System para imprimir a mensagem 
+         *(Dica: digite sout e pressione TAB para gerar a instrução println).
+         */
+        System.out.println("Bem vindo!");
+    }
+}
+
+//4. Execute a classe e observe o resultado. (SHIFT + F6)
